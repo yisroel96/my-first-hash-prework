b25faeb1bb1700e8aa89f22600e97bfc8b1bb9df
witty-collection-3981 <yisroel96@gmail.com>
2018-11-27 21:44:08 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-
+the_hash = {"program" => "awesome!"}
 end
 
 
@@ -9,7 +9,12 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
+  manifest_value = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
 
+  }
 
 end
 
@@ -40,5 +45,5 @@ def adding
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
